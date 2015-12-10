75b0f3c23d975e79f8bd1658e7088ce1e13c2bb9
duwls997 <student@T18-11.et.qcc.edu>
2015-12-10 16:54:03 -0500

    calss
    finish

---
+++ js/foundation/_vti_cnf/foundation.topbar.js
@@ -0,0 +1,11 @@
+vti_encoding:SR|utf8-nl
+vti_author:SR|ET\\Instructor
+vti_modifiedby:SR|ET\\student
+vti_timelastmodified:TR|10 Dec 2015 19:39:22 -0000
+vti_timecreated:TR|17 Sep 2015 20:52:50 -0000
+vti_extenderversion:SR|12.0.0.0
+vti_backlinkinfo:VX|
+vti_cacheddtm:TX|10 Dec 2015 19:39:22 -0000
+vti_filesize:IR|15629
+vti_syncofs_146.111.120.96\:80/fa15/et_710_fn1/ywang79:TW|10 Dec 2015 21:50:59 -0000
+vti_syncwith_146.111.120.96\:80/fa15/et_710_fn1/ywang79:TX|10 Dec 2015 19:39:22 -0000
